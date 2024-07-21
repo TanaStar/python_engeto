e0b76c1020a8fbd697a495f1e6f4c9b973775fc0
Tatiana Staroňová <tatiana@staronova.me>
2024-07-21 16:21:07 +0200

    projekt_1 zmeny (prihlasenie, statistika)

--- projekt_1.py
+++ projekt_1.py
@@ -36,91 +36,89 @@ garpike and stingray are also present.'''
 import sys
 from collections import Counter
 from operator import itemgetter
+import string
 
 username = input("Prihlasovaci jmeno:")
-password = input('Heslo:')
-
 known_users ={'bob': '123', 'ann': 'pass123', 
              'mike': 'password123','liz': 'pass123'}
 
-print(f'username: {username}')
-print(f'password: {password}')
 print('-'*40)
 
 # overenie uzivatela
 
-while username not in known_users:
-    print('unregistered user, terminating the program') 
-    break
+if username not in known_users:
+    print('Unregistered user, terminating the program')
+    sys.exit(1)
+
+# overenie hesla
+
+password = input('Heslo:')
+saved_pass = known_users[username]
+if saved_pass == password:
+    print(f'Welcome to the app, {username}')
+    print(f'We have {len(TEXTS)} texts to be analyzed.')
+    print('-'*40) 
 else:
-    saved_pass = known_users[username]
-    if saved_pass == password:
-        print(f'Welcome to the app, {username}')
-        print('We have 3 texts to be analyzed.')
-        print('-'*40) 
+    print('Incorrect password, terminating the program')
+    sys.exit(2)
 
 
 # vybratie textu na analyzu
 
-    text = input('Enter a number btw. 1 and 3 to select:')
+text = input(f'Enter a number btw. 1 and {len(TEXTS)} to select:')
 
-    
-    if not text.isnumeric() or int(text) not in range(1,4):
-        print('Wrong entry')
-        sys.exit(1)
 
-    print(f'Enter a number btw. 1 and 3 to select: {text}')
-    print('-'*40)
+if not text.isnumeric() or int(text) not in range(1,len(TEXTS)+1):
+    print('Wrong entry')
+    sys.exit(3)
 
-    text_numb = int(text)
-    selected_text = TEXTS[text_numb-1]
+print('-'*40)
+
+selected_text = TEXTS[int(text)-1]
 
 # ocistenie textu
 
-    clean_words = []
+clean_words = [word.strip(string.punctuation) for word in selected_text.split()]
 
-    for word in selected_text.split():
-        clean_words.append(word.strip(',.'))
 
 # statistika
 
-    word_count = len(clean_words)
-    print(f'There are {word_count} words in selected text.')
+word_count = len(clean_words)
+print(f'There are {word_count} words in selected text.')
 
-    starts_upper = 0    
-    all_upper = 0
-    all_lower = 0
-    numbers = []
-    for word in clean_words:
+starts_upper = 0    
+all_upper = 0
+all_lower = 0
+numbers = []
+for word in clean_words:
+    if word.isalpha():
         if word.isupper():
             all_upper += 1
-        elif word.islower():
+        if word.islower():
             all_lower += 1
-        elif word.istitle():
+        if word[0].isupper():
             starts_upper += 1
-        elif word.isnumeric():
-            numbers.append(int(word))
-    print(f'There are {starts_upper} titlacase words.')
-    print(f'There are {all_upper} uppercase words.')
-    print(f'There are {all_lower} lowercase words.')
-    print(f'There are {len(numbers)} numeric strings.')
-
-    summ = sum(numbers)
-    print(f'The sum of all numbers {summ}.')
+    elif word.isnumeric():
+        numbers.append(int(word))
+print(f'There are {starts_upper} titlecase words.')
+print(f'There are {all_upper} uppercase words.')
+print(f'There are {all_lower} lowercase words.')
+print(f'There are {len(numbers)} numeric strings.')
+print(f'The sum of all numbers {sum(numbers)}.')
 
 # histogram
 
-    word_lenght = Counter(len(words) for words in (clean_words))
-    sorted_word_lenght = sorted(word_lenght.items())
+word_length = Counter(len(words) for words in (clean_words))
+sorted_word_length = sorted(word_length.items())
 
-    maximum = max(sorted_word_lenght, key = itemgetter(1))[1]
+maximum = max(sorted_word_length, key = itemgetter(1))[1]
 
-    print('-'*40)
-    print(f'LEN| {"OCCURENCES".center(maximum)} |NR.')
-    print('-'*40)
+print('-'*40)
+print(f'LEN| {"OCCURENCES".center(maximum)} |NR.')
+print('-'*40)
 
-    for i in sorted_word_lenght:
-        a = i[0]
-        b = i[1]
-        print(f'{a:>2} | {b*"*":<{maximum}} | {b}')
+for i in sorted_word_length:
+    a = i[0]
+    b = i[1]
+    print(f'{a:>2} | {b*"*":<{maximum}} | {b}')
     
